4741ae990dddfbbf6960678d048597684b1ecfec
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-09-25 08:10:35 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C="denuncia-resalto-v2";
+const F=["./","index.html","manifest.json","p0.jpg","p12.jpg","resalta.jpg","p72.jpg","poster.jpg","og.jpg","icon-192.png","icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
